b7397e73464244acec2e5b14feed3fdbc0c00165
Nadeem Haidar <nadeem.haidar@mail.utoronto.ca>
2015-11-24 01:13:37 -0500

    Switched to google cloud server + added csl server url cfg file

--- chrome/content/zoteroEXTended/cslEdit.js
+++ chrome/content/zoteroEXTended/cslEdit.js
@@ -6,9 +6,17 @@ Zotero.cslEdit = {
 	
 	openWindow: function() {
 		
+		var request = new XMLHttpRequest();
+		request.open('GET', "chrome://zoteroEXTended/content/csl_server_url.cfg", false);
+		request.send(null);
+		var server_url= "http://104.196.43.156:5001/about/"; // fallback server URL;
+		if (request.status == 200){
+			server_url = request.responseText;
+		}
+		
 		var strWindowFeatures = "resizable=yes,chrome=yes,centerscreen=yes";
 		this.cslWindow = window.openDialog("chrome://zoteroEXTended/content/browser.xul",
-					"basicViewer", "chrome,resizable,centerscreen,menubar,scrollbars", "http://54.84.6.245:5001/about/");		
+					"basicViewer", "chrome,resizable,centerscreen,menubar,scrollbars", server_url);		
 	},
 };
 
